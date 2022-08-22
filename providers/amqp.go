e9afb0740c9215aa880ffcfac1cae25892d2065a
clemon <cjw7360chen@gmail.com>
2022-08-22 22:45:47 +0800

    basic arch

--- providers/amqp.go
+++ providers/amqp.go
@@ -1,12 +1,33 @@
 package providers
 
 import (
+	"sync"
+
+	"github.com/c1emon/barkbridge/barkserver"
 	amqp "github.com/rabbitmq/amqp091-go"
-	"github.com/sirupsen/logrus"
 )
 
-func Dail() {
-	// amqp.Dial("")
-	logrus.Debug()
-	amqp.Dial("")
+type AmqpProvider struct {
+	Addr      string
+	ProvideCh chan barkserver.Message
+	stopCh    chan any
+	wg        *sync.WaitGroup
+}
+
+func (p *AmqpProvider) Start() {
+	amqp.Dial(p.Addr)
+}
+
+func (p *AmqpProvider) Stop() {
+	close(p.stopCh)
+	p.wg.Wait()
+}
+
+func (p *AmqpProvider) GetCh() <-chan barkserver.Message {
+
+	return p.ProvideCh
+}
+
+func (p *AmqpProvider) GetName() string {
+	return ""
 }
